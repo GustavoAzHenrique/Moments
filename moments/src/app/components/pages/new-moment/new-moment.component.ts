9811ecc0dd2cbb82755d47e237b2a3536c392d27
GustavoAzHenrique <92117675+GustavoAzHenrique@users.noreply.github.com>
2022-08-11 15:30:30 -0300

    Inserir dados na API - ok

---
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent implements OnInit {
+  btnText = 'Compartilhar!';
+
+  constructor(private momentService: MomentService) { }
+
+  ngOnInit(): void {
+  }
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    //todo
+
+    this.momentService.createMoment(formData).subscribe();
+    
+    //exibir msg
+    //redirect
+  }
+
+}
